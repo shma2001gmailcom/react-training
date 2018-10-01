9f2d0d44606ee5e0c96af5ddd86373a1be8e3177
shma2001gmailcom <shma2001@gmail.com>
2018-10-01 12:15:57 +0300

    -

--- src/Incrementer.js
+++ src/Incrementer.js
@@ -1,30 +1,39 @@
-import React, {Component} from 'react';
+import React, {
+  Component
+} from "react";
 
 class Incrementer extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {temperature: 0};
-    }
-
-    onIncrease() {
-      this.setState(prevState => ({
-        temperature: prevState.temperature + 1
-      }))
-    }
-
-    onDecrease(){
-      this.setState(prevState => ({
-        temperature: prevState.temperature - 1
-      }))
-    }
-
-    render() {
-      return (
-        <div>
-          <h2>Current value: {this.state.temperature}</h2>
-          <button onClick={(event) => this.onDecrease(event)}>-</button>
-          <button onClick={(event) => this.onIncrease(event)}>+</button>
-        </div>);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
   }
-  export default Incrementer
+
+  onIncrease() {
+    this.setState(prevState => ({
+      value: prevState.value + 1
+    }));
+  }
+
+  onDecrease() {
+    this.setState(prevState => ({
+      value: prevState.value - 1
+    }));
+  }
+
+  render() {
+    return ( <div>
+      <h2> Current value: {
+        this.state.value
+      } 
+      </h2> <button onClick = {
+        event => this.onDecrease(event)
+      } > - </button> <
+      button onClick = {
+        event => this.onIncrease(event)
+      } > + </button> </ div >
+    );
+  }
+}
+export default Incrementer;
