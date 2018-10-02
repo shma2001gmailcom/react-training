d363f6dc7aa2bb31d27f0bfd0066059906cb1354
shma2001gmailcom <shma2001@gmail.com>
2018-10-02 14:16:23 +0300

    reformat

--- src/App.js
+++ src/App.js
@@ -4,32 +4,26 @@ import './App.css';
 import Timer from './Timer'
 import Incrementer from './Incrementer'
 import Clocks from './Clocks'
-import axios from 'axios'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {username : ''};
-    
   }
 
   handleClick() {
-    var x;
-    fetch('http://localhost:8080/react-back/index')
-    .then(response => response.json())
-    .then(data => this.setState({
-      username: data.name
-    }));
+    fetch('http://localhost:8080/react-back/index')//url to fetch
+     .then(response => response.json())
+     .then(data => this.setState({
+       username: data.name
+      }));
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1> */}
-        </header>
         <div className='button__container'>
-          <button className='button' onClick={() => this.handleClick()}>Click Me</button>
+          <button className='button' onClick={() => this.handleClick()}>fetch</button>
         </div>
         <p>{this.state.username}</p>
         <Timer/>
