c2640d3d95b32f96c9b7eaacb30dbae744eda0e3
shma2001gmailcom <shma2001@gmail.com>
2018-10-02 14:51:33 +0300

    submit data to server and fetch result based on it

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ import Clocks from './Clocks'
 class App extends Component {
   constructor() {
     super();
-    this.state = {username : ''};
+    this.state = {
+      username : '',
+      value:''
+    };
   }
 
+  handleChange(event) {
+    this.setState({value: event.target.value, username: this.state.username});
+  }
+  
   handleClick() {
-    fetch('http://localhost:8080/react-back/index')//url to fetch
+    fetch('http://localhost:8080/react-back/index?value='+ this.state.value)//url to fetch
      .then(response => response.json())
      .then(data => this.setState({
+       value: this.state.value,
        username: data.name
       }));
   }
@@ -23,7 +31,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className='button__container'>
-          <button className='button' onClick={() => this.handleClick()}>fetch</button>
+          <div><input className='input' width='50' value={this.state.value} onChange={(event) => this.handleChange(event)}/> </div>
+          <div><button className='button' onClick={() => this.handleClick()}>fetch</button></div>
         </div>
         <p>{this.state.username}</p>
         <Timer/>
